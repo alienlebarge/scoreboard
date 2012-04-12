1d9d9220e9bf3298e0099f370bc12df89a3dc6b6
alienlebarge <cedric.aellen@alienlebarge.ch>
2012-04-12 10:38:51 +0200

    score increase and deacrease

--- scoreboard/js/script.js
+++ scoreboard/js/script.js
@@ -8,28 +8,62 @@ function writeScore(team, score) {
 
 }
 
+function getScore () {
+    var homeScore;
+    var awayScore;
+
+    // check if homeScore allready exist
+    if (localStorage.getItem("homeScore")) {
+        // if yes, we get the stored score
+        homeScore = localStorage.getItem('homeScore');
+    } else {
+        // if note we set it to 0
+        homeScore = 0;
+        localStorage.setItem('homeScore', homeScore);
+    }
+
+    // check if awayScore allready exist
+    if (localStorage.getItem("awayScore")) {
+        // if yes, we get the stored score
+        awayScore = localStorage.getItem('awayScore');
+    } else {
+        // if note we set it to 0
+        awayScore = 0;
+        localStorage.setItem('awayScore', awayScore);
+    }
+
+    writeScore('home', homeScore);
+    writeScore('away', awayScore);
+}
+
 function init() {
     // initialisation
-    var homeScore = 0;
-    var awayScore = 0;
-
+    getScore();
 
     // event listener
     document.getElementById('home_scorePlus').onclick = function () {
-        homeScore++;
-        writeScore('home', homeScore);
+        score = JSON.parse(localStorage.getItem('homeScore'));
+        score++;
+        localStorage.setItem('homeScore', JSON.stringify(score));
+        writeScore('home', score);
     };
     document.getElementById('home_scoreMinus').onclick = function () {
-        homeScore--;
-        writeScore('home', homeScore);
+        score = JSON.parse(localStorage.getItem('homeScore'));
+        score--;
+        localStorage.setItem('homeScore', JSON.stringify(score));
+        writeScore('home', score);
     };
     document.getElementById('away_scorePlus').onclick = function () {
-        awayScore++;
-        writeScore('away', awayScore);
+        score = JSON.parse(localStorage.getItem('awayScore'));
+        score++;
+        localStorage.setItem('awayScore', JSON.stringify(score));
+        writeScore('away', score);
     };
     document.getElementById('away_scoreMinus').onclick = function () {
-        awayScore--;
-        writeScore('away', awayScore);
+        score = JSON.parse(localStorage.getItem('awayScore'));
+        score--;
+        localStorage.setItem('awayScore', JSON.stringify(score));
+        writeScore('away', score);
     };
 }
 
